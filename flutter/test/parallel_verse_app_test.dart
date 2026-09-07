5f943a41aefdb0c32ae0290a0c1645eb37d9cd16
tharun-extinct <tharunkumar7041@gmail.com>
2026-09-08 01:49:32 +0530

    Add widget test for empty instance state

---
+++ flutter/test/parallel_verse_app_test.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:parallel_verse_controller/main.dart';
+
+void main() {
+  testWidgets('shows the empty instance state', (tester) async {
+    await tester.pumpWidget(
+      const ProviderScope(child: ParallelSpaceApp()),
+    );
+
+    expect(find.text('Instances'), findsOneWidget);
+    expect(find.text('No instances yet'), findsOneWidget);
+  });
+}
